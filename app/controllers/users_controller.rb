12e9a02f0deea47d93631290ef7cda4181ec0bfe
Joe Hunt <Joseph.Hunt@intridea.com>
2011-08-22 18:30:48 -0400

    ticket 32; added views for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,39 @@
 class UsersController < ApplicationController
+  
+  before_filter :fetch_user, :only => ["edit", "show", "destroy"]
+  
   def index
+    @users = User.all
   end
   
   def show
   end
   
   def new
+    @user = User.new
   end
   
   def create
+    @user = User.create params[:user]
+    if @user.save
+      redirect_to show @user
+    else
+      render "edit"
+    end
   end
   
   def edit
   end
   
   def update
+    @user = User.update_attributes params[:user]
   end
   
   def destroy
+  end
+  
+  private
+  def fetch_user
+    @user = User.find params[:id]
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   has_and_belongs_to_many :extracurriculars
+  
+  validates_presence_of :name
 end
