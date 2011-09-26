4eb578ee7143718c3bb372d83842cf93f6b39031
Jake Mauer <jake@jakemauer.com>
2011-09-26 01:51:33 -0700

    Added dedicated twitter dropdown js file

---
+++ app/assets/javascripts/dropdowns.js
@@ -0,0 +1,30 @@
+!function( $ ){
+
+  var d = 'a.menu, .dropdown-toggle'
+
+  function clearMenus() {
+    $(d).parent('li').removeClass('open')
+  }
+
+  $(function () {
+    $('html').bind("click", clearMenus)
+    $('body').dropdown( '[data-dropdown] a.menu, [data-dropdown] .dropdown-toggle' )
+  })
+
+  /* DROPDOWN PLUGIN DEFINITION
+   * ========================== */
+
+  $.fn.dropdown = function ( selector ) {
+    return this.each(function () {
+      $(this).delegate(selector || d, 'click', function (e) {
+        var li = $(this).parent('li')
+          , isActive = li.hasClass('open')
+
+        clearMenus()
+        !isActive && li.toggleClass('open')
+        return false
+      })
+    })
+  }
+
+}( window.jQuery || window.ender );
