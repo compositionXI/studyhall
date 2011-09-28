e03317088622f49d37b16265b56cda25d68aa4b6
Joe Hunt <Joseph.Hunt@intridea.com>
2011-09-28 11:01:01 -0400

    homepage and profile page updates

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,12 @@ module UsersHelper
   def user_protected_profile?(user, attrubute)
     User::PROTECTED_PROFILE_ATTRBUTES.include? attrubute.to_s
   end
+  
+  def greek_affiliation
+     @user.male? ? "#{@user.fraternity}" : "#{@user.sorority}"
+  end
+  
+  def sybling_type
+    @user.male? ? "brother" : "sister"
+  end
 end
