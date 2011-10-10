28cbc26331876be761b4bc7b9b3bb9457334c5e4
Ian Yang <me@iany.me>
2011-10-10 21:48:55 +0800

    Use template for password reset mail (refs #65)

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -8,13 +8,14 @@ class Notifier < ActionMailer::Base
     mail(:to => user.email, :bcc => ["bcc@example.com", "Order Watcher <watcher@example.com>"])
   end
 
-  def password_reset_instructions(user)  
+  def password_reset_instructions(user)
+    @user = user
+    @url = edit_password_reset_url(@user.perishable_token)
     mail(
       :subject => "Password Reset Instructions",
       :from => "StudyHall.com",
-      :to => user.email,
-      :date => Time.now, 
-      :body => edit_password_reset_path(user.perishable_token)
+      :to => @user.email,
+      :date => Time.now
     )
   end
 end
