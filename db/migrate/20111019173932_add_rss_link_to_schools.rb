72e64d9a7466ed90e05379d2c3ff9bd14e0c65d9
Joe Hunt <Joseph.Hunt@intridea.com>
2011-10-19 14:01:00 -0400

    fixed migration for schools rss_link, domain_name, and active

--- db/migrate/20111019173932_add_rss_link_to_schools.rb
+++ db/migrate/20111019173932_add_rss_link_to_schools.rb
@@ -1,5 +1,7 @@
 class AddRssLinkToSchools < ActiveRecord::Migration
   def change
     add_column :schools, :rss_link, :string
+    add_column :schools, :domain_name, :string
+    add_column :schools, :active, :boolean
   end
 end
