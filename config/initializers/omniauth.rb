c1dd393572073980fd222e5218669fa004f1a84e
Brent Collier <brentmc79@gmail.com>
2011-10-24 17:07:54 -0400

    Add facebook app config to config.yml and update the omniauth initializer to use the APP_CONFIG

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # this is just for test and only allow redirect to localhost:3000
-  provider :facebook, '239998316048833', '3c191804c8169e53b0e182210010dcf7'
+  provider :facebook, APP_CONFIG["facebook"]["app_id"], APP_CONFIG["facebook"]["app_secret"]
 end 
