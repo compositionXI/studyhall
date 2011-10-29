22f52a9710715f0a7789e74364a372e1db422402
Joe Hunt <Joseph.Hunt@intridea.com>
2011-10-29 00:21:48 -0400

    fixed merge error

--- db/schema.rb
+++ db/schema.rb
@@ -111,7 +111,6 @@ ActiveRecord::Schema.define(:version => 20111028181149) do
     t.boolean  "deleted",                   :default => false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "parent_id"
     t.string   "attachment_file_name"
     t.string   "attachment_content_type"
     t.integer  "attachment_file_size"
@@ -241,7 +240,6 @@ ActiveRecord::Schema.define(:version => 20111028181149) do
     t.string   "perishable_token"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "role"
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
