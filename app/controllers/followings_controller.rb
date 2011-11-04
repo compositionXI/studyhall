3e41fa01c8ae1f5a044f76a232c951ab53551614
yincan <yincan@intridea.com>
2011-11-04 16:34:33 +0800

    using coffee script on follow/unfollow..learning

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -8,11 +8,13 @@ class FollowingsController < ApplicationController
       following = current_user.follow!(followed_user)
       @success = following.valid?
       followed_user.deliver_followed_notification!(current_user) if followed_user.notify_on_follow? and @success
+      render :json => @success
     end
   end
 
   def destroy
     @success = current_user.unfollow!(nil,{:followed_user_id => params[:id]})
+    render :json => (@success == 1)
   end
 
 end
