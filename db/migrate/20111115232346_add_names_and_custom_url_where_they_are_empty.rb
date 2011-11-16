bd8413c36a55eb3a665425dab4c53338a53733a1
Brent Collier <brentmc79@gmail.com>
2011-11-15 20:23:18 -0500

    fix the name/custom_url migration

--- db/migrate/20111115232346_add_names_and_custom_url_where_they_are_empty.rb
+++ db/migrate/20111115232346_add_names_and_custom_url_where_they_are_empty.rb
@@ -2,13 +2,10 @@ class AddNamesAndCustomUrlWhereTheyAreEmpty < ActiveRecord::Migration
   def up
     User.reset_column_information
     User.all.each do |user|
-      user.first_name = "John" if user.first_name.nil? || user.first_name.empty?
-      user.last_name = "Doe" if user.last_name.nil? || user.last_name.empty?
-      if user.custom_url.nil? || user.custom_url.empty?
-        salt = User.where(:first_name => "John", :last_name => "Doe").count + 1
-        user.custom_url = user.name+salt.to_s 
-      end
-      user.save!
+      user.first_name = "John" if user.first_name.blank?
+      user.last_name = "Doe" if user.last_name.blank?
+      user.custom_url = "user#{user.id}" if user.custom_url.blank?
+      user.save(perform_validation: false)
     end
   end
   
