88435d749059bd24791057079cbecf1523baf551
Peter Jackson <pete@peteonrails.com>
2012-01-09 15:38:10 -0500

    Add view specs for classes, placeholder tests

---
+++ spec/views/classes/show.html.erb_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe "classes/show.html.erb" do
+  it "should not blow up when there are deleted users in the class with posts on the page" do 
+    pending "but it does not"
+  end
+end
