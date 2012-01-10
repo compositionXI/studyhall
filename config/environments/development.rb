2910afb86d91bb0dd23e67ff855193aa0eeb69e0
Andy Wang <wangyaodi@gmail.com>
2012-01-10 19:33:23 +0800

    add observer to deal with cache expirations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ Studyhall::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
   
   # Use a different cache store in production
   config.cache_store = :dalli_store, 'localhost:11211'
