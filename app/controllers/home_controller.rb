300ed022afe59e92a4da7f24e33fdfb854fe0064
Andy Wang <wangyaodi@gmail.com>
2012-01-11 11:57:55 +0800

    preload course list to speed up the js

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,10 @@ class HomeController < ApplicationController
   def index
     @notebooks = @current_user.notebooks
     @unsorted_notes = @current_user.notes.unsorted
+    @enrollment = Enrollment.new
+    @offerings = current_user.school.offerings.includes(:course, :instructor)
+    @user = @current_user
+    @modal_link_id = "add_course_button"
     unless current_user.profile_complete?
       flash.now[:notice] = "Your profile is #{current_user.profile_completion_percentage}% complete!"
     end
