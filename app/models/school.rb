77f2160d07e9fa4874f9fadb7b1c93d7a70df6a7
Andy Wang <wangyaodi@gmail.com>
2012-01-12 22:21:26 +0800

    update from_email method to deal with different school assignment case

--- app/models/school.rb
+++ app/models/school.rb
@@ -6,6 +6,9 @@ class School < ActiveRecord::Base
   has_many :users
   has_many :rss_entries
   
+  scope :active_schools, where(:active => true)
+  scope :inactive_schools, where(:active => false)
+  
   validates_presence_of :name
   validates_uniqueness_of :name 
   
@@ -14,12 +17,17 @@ class School < ActiveRecord::Base
   def latest_news
     rss_entries.limit(10).all
   end
+  
   def self.from_email(email)
     domain = email.split("@")[1] rescue nil
-    found_school = School.find_by_domain_name(domain)
-    return found_school if found_school
-    if Rails.env.development? || Rails.env.test? || Rails.env.staging?
-      School.last
+    if (Rails.env.development? || Rails.env.test? || Rails.env.staging?) and (domain == "intridea.com" || domain == "studyhall.com")
+      School.active_schools.first
+    elsif (active_school = School.active_schools.find_by_domain_name(domain))
+      active_school
+    elsif (inactive_school = School.inactive_schools.find_by_domain_name(domain))
+      inactive_school
+    elsif domain.is_a?(String) and domain.include?(".edu")
+      inactive_school = School.inactive_schools.create!(:name => "Unknow School", :domain_name => domain)
     else
       nil
     end
