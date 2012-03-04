f4068ecd01374f2a9167b1611013420d9f7cd713
sam grondahl <sam@studyhall.com>
2012-03-03 23:18:15 -0500

    update cal model

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,2 +1,9 @@
 class Calendar < ActiveRecord::Base
+
+  include Ownable
+
+  belongs_to :user
+  belongs_to :study_session
+  attr_accessible :user_id, :time_start, :time_end, :date_start, :date_end, :study_session_id, :schedule_id
+
 end
