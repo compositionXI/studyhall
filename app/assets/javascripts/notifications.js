26518cbc7f98d2f324d23dea3f3c0f58cab3c7cb
Tom Lemberg <tlemberg10@gmail.com>
2012-04-19 19:18:10 -0400

    removed stray file

--- app/assets/javascripts/notifications.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  var faye_client = new Faye.Client('http://app02.c45577.blueboxgrid.com:9292/faye');
-  faye_client.subscribe('/notifications/push', function(data) {
-    $("#notifications_queue").append(data);
-	});
-  $("#notifications_queue").css("width", "200px");
-});
