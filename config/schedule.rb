8d4e22af181ef6ef221b80edde92d766ae2a04f5
sam grondahl <sam@studyhall.com>
2012-04-24 12:08:02 -0400

    update runner cron w rvm wrapper

--- config/schedule.rb
+++ config/schedule.rb
@@ -22,7 +22,7 @@
 set :environment, 'production'
 
 job_type :rake,    "cd :path && RAILS_ENV=:environment rvm_trust_rvmrcs_flag=1 bundle exec rake :task --silent :output"
-job_type :runner, "cd :path && script/rails runner -e :environment ':task' :output"
+job_type :runner, "cd :path && rvm_trust_rvmrcs_flag=1 studyhall_rails runner -e :environment ':task' :output"
 
 every 12.hour do
   rake "campus_news:fetch"
@@ -36,7 +36,7 @@ every 24.hours do
   rake "studyhall:remove_old_searches"
 end
 
-every 12.hours do
+every :hour do
   runner "Recommendation.list_all"
 end
 
