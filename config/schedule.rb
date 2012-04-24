93b862ff54d75fe3c08eb8c34498a51708a49579
sam grondahl <sam@studyhall.com>
2012-04-24 16:59:21 -0400

    update schedule flag rvmrc

--- config/schedule.rb
+++ config/schedule.rb
@@ -22,7 +22,7 @@
 set :environment, 'production'
 
 job_type :rake,    "cd :path && RAILS_ENV=:environment rvm_trust_rvmrcs_flag=1 bundle exec rake :task --silent :output"
-job_type :runner, "cd :path && rvm_trust_rvmrcs_flag=1 studyhall_rails runner -e :environment ':task' :output"
+job_type :runner, "rvm_trust_rvmrcs_flag=1 cd :path && studyhall_rails runner -e :environment ':task' :output"
 
 every 12.hour do
   rake "campus_news:fetch"
