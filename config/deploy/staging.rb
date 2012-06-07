109b2ecb72eaa8aed752db06d3a324c1c551b9fd
sam grondahl <sam@studyhall.com>
2012-06-06 21:47:27 -0400

    update staging deploy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,4 +4,4 @@ set :rails_env, 'staging'
 set :bundle_without, [:development, :test]
 #set :branch, 'andy_demo_chat_on_envolve'
 
-server "stage01.45469.blueboxgrid.com", :app, :web, :db, :primary => true 
+server "staging01.c45577.blueboxgrid.com", :app, :web, :db, :primary => true 
